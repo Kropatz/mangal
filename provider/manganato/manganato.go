22808eaf3c4655d1cb6a8c517cf039aea1f65497
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-02-03 22:51:25 +0100

    fix: manganato

--- provider/manganato/manganato.go
+++ provider/manganato/manganato.go
@@ -25,7 +25,7 @@ var Config = &generic.Configuration{
 		query = strings.TrimSpace(query)
 		query = strings.ToLower(query)
 		query = url.QueryEscape(query)
-		template := "https://chapmanganato.com/" + baseUrl + "/search/story/%s"
+		template := "https://manganato.com/search/story/%s"
 		return fmt.Sprintf(template, query)
 	},
 	MangaExtractor: &generic.MangaExtractor{
